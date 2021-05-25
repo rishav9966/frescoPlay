8319ca26a1e2c1bc38e728156109d5b8efd73eec
rishav9966 <39216235+rishav9966@users.noreply.github.com>
2021-05-25 23:42:16 +0530

    Create Hashset.java

---
+++ Miniproject - Java Complete Beginner/Hashset.java
@@ -0,0 +1,10 @@
+package com.fresco;
+
+public class Hashset
+{
+    public static String getOut(int numberOfMatches, String squads, int squad1, int squad2)
+    {
+        //write your code here
+        return null;
+    }
+}
