a05edeaf321726e8df5c1543c404c529368da2dd
rishav9966 <39216235+rishav9966@users.noreply.github.com>
2021-05-28 01:46:39 +0530

    Create TreemapHandson.java

---
+++ Miniproject - Java Complete Beginner/TreemapHandson.java
@@ -0,0 +1,46 @@
+package com.fresco;
+
+import java.util.*;
+
+
+
+public class TreemapHandson {
+    
+    public TreeMap<Integer,String> createPlayerPositionMap(String cricketDataset)
+    {
+        
+        return null;
+        
+    }
+    public TreeMap<String,Integer> createPlayerScoreMap(String cricketDataset)
+    {
+
+        return null;
+        
+    }
+    public TreeMap<?,?> createMatchesMap(String cricketDataset)
+    {
+           
+        return null;
+    }
+    
+    
+    public String getQuery(String cricketDataset,String query)
+    {
+        
+        return null;
+    }
+    
+    
+    
+    
+    
+}
+            
+
+        
+        
+        
+        
+        
+
